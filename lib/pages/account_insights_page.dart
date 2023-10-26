926623144ca50383f5c0e396f5e77b765d5b9709
Mystoryme <79404492+Mystoryme@users.noreply.github.com>
2023-10-26 21:54:19 +0700

    fix: dashboard

--- lib/pages/account_insights_page.dart
+++ lib/pages/account_insights_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:picme/pages/profile_page.dart';
-import 'package:picme/widget/account_insights/detail_insights.dart';
+import 'package:picme/widget/account_insights/dashboard_engaged.dart';
+import 'package:picme/widget/account_insights/dashboard_reach.dart';
 import 'package:picme/widget/account_insights/overview_account.dart';
 import 'package:picme/widget/profile/profilesec.dart';
 
@@ -13,57 +14,60 @@ class AccountInsightsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          padding: const EdgeInsets.all(15.0),
-          // alignment: Alignment.center,
-          child: Column(
-            // mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Row(
-                // mainAxisAlignment: MainAxisAlignment.start,
-                // crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Expanded(
-                    flex: 1,
-                    child: Container(
-                      alignment: Alignment.bottomLeft,
-                      child: IconButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const ProfilePage()),
-                          );
-                        },
-                        icon: const Icon(
-                          Icons.arrow_back_ios_new_rounded,
-                          color: Colors.black54,
+        child: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.all(15.0),
+            // alignment: Alignment.center,
+            child: Column(
+              // mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  // mainAxisAlignment: MainAxisAlignment.start,
+                  // crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      flex: 1,
+                      child: Container(
+                        alignment: Alignment.bottomLeft,
+                        child: IconButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const ProfilePage()),
+                            );
+                          },
+                          icon: const Icon(
+                            Icons.arrow_back_ios_new_rounded,
+                            color: Colors.black54,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  // const SizedBox(
-                  //   height: 150,
-                  // ),
-                  Expanded(
-                    flex: 4,
-                    child: Container(
-                      padding: const EdgeInsets.only(left: 10),
-                      child: Text(
-                        'Account Insights',
-                        style: GoogleFonts.poppins(
-                          color: Colors.black,
-                          fontSize: 24,
-                          fontWeight: FontWeight.normal,
+                    // const SizedBox(
+                    //   height: 150,
+                    // ),
+                    Expanded(
+                      flex: 4,
+                      child: Container(
+                        padding: const EdgeInsets.only(left: 10),
+                        child: Text(
+                          'Account Insights',
+                          style: GoogleFonts.poppins(
+                            color: Colors.black,
+                            fontSize: 24,
+                            fontWeight: FontWeight.normal,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-              const OverviewAccount(),
-              const DetailInsights(),
-            ],
+                  ],
+                ),
+                const OverviewAccount(),
+                DashBoardReach(),
+                DashBoardEngaged(),
+              ],
+            ),
           ),
         ),
       ),
