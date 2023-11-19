a0865527f6246dd51344285c77fd5cf3996cf1b8
Mystoryme <79404492+Mystoryme@users.noreply.github.com>
2023-11-20 00:54:05 +0700

    fix: postcard_grid

--- lib/widget/postcard/postcard_grid.dart
+++ lib/widget/postcard/postcard_grid.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:picme/models/gridpost.dart';
 import 'package:picme/models/gridposts.dart';
+import 'package:picme/pages/grid_click_page.dart';
 
 class PostCardGrid extends StatefulWidget {
   const PostCardGrid({Key? key, required this.posts}) : super(key: key);
@@ -14,24 +15,51 @@ class _PostCardGridState extends State<PostCardGrid> {
   GridPost? post;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.maxFinite,
-      height: double.maxFinite,
+    return Expanded(
       child: GridView.builder(
-          itemCount: widget.posts.posts.length,
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 3, mainAxisSpacing: 1.5, crossAxisSpacing: 1.5),
-          itemBuilder: (context, index) {
-            return Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: NetworkImage(widget.posts.posts[index].imageUrl),
-                  fit: BoxFit.contain,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          childAspectRatio: 1.0,
+        ),
+        itemCount: widget.posts.posts?.length ?? 0,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => GridClickPage(
+                      postId: widget
+                          .posts.posts[index].postId, onDelete: () {  },),
+                       // Replace with your page
                 ),
-              ),
-              height: 194.5,
-            );
-          }),
+              );
+            },
+            child: Image.network(
+              widget.posts.posts?[index]?.imageUrl ?? '',
+              fit: BoxFit.cover,
+            ),
+          );
+        },
+      ),
     );
+    // return Container(
+    //   width: double.maxFinite,
+    //   height: double.maxFinite,
+    //   child: GridView.builder(
+    //       itemCount: widget.posts.posts.length,
+    //       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+    //           crossAxisCount: 3, mainAxisSpacing: 1.5, crossAxisSpacing: 1.5),
+    //       itemBuilder: (context, index) {
+    //         return Container(
+    //           decoration: BoxDecoration(
+    //             image: DecorationImage(
+    //               image: NetworkImage(widget.posts.posts[index].imageUrl),
+    //               fit: BoxFit.contain,
+    //             ),
+    //           ),
+    //           height: 194.5,
+    //         );
+    //       }),
+    // );
   }
 }
--- lib/widget/search/grid_photo.dart
+++ lib/widget/search/grid_photo.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:picme/models/boost.dart';
 import 'package:picme/models/boosts.dart';
+import 'package:picme/models/post.dart';
+import 'package:picme/pages/post_click_page.dart';
 
 class GridPhoto extends StatefulWidget {
   const GridPhoto({Key? key, required this.posts}) : super(key: key);
@@ -15,7 +17,7 @@ class _GridPhotoState extends State<GridPhoto> {
   Widget build(BuildContext context) {
     return Expanded(
       child: GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           childAspectRatio: 1.0,
         ),
@@ -23,12 +25,12 @@ class _GridPhotoState extends State<GridPhoto> {
         itemBuilder: (context, index) {
           return GestureDetector(
             onTap: () {
-              // Navigator.of(context).push(
-              //   MaterialPageRoute(
-              //     builder: (context) =>
-              //         AccountProfilePage(), // Replace with your page
-              //   ),
-              // );
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) =>
+                      PostClickPage(postId: widget.posts.posts[index].postId), // Replace with your page
+                ),
+              );
             },
             child: Image.network(
               widget.posts.posts?[index]?.imageUrl ?? '',
